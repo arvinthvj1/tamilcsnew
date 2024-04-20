99a48fc2abe2701747bc5d0e0005dcb1e0f85a16
Arvinth <amanivannaboopathy@inniveinc.com>
2024-04-20 17:52:04 +0530

    changes

--- src/fe-handlers/requestHandlers.js
+++ src/fe-handlers/requestHandlers.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/api/crud/allCrud';
+const BASE_URL = window.location.origin+'/api/crud/allCrud';
 
 export const fetchData = async (collectionName, pipeline) => {
   console.log("pipeline",pipeline)
